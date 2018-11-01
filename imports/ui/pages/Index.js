14933c215084d12bdb8be529c2ee8cf5d3f5ddbf
Patrick Gilder <pgilder@cyberitas.com>
2018-11-01 13:04:22 -0700

    Stripe Styling Added

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -6,8 +6,9 @@ import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
 
 
 const Index = () => (
-  <div className="Index bg-1">
-    <Jumbotron className="text-center brand-overlay">
+  <div className="Index">
+    <div className="bg-1 stripes">
+    <Jumbotron className="text-center brand-overlay stripes-offset">
       <div className="pool-icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 21c-1.11 0-1.73-.37-2.18-.64-.37-.22-.6-.36-1.15-.36-.56 0-.78.13-1.15.36-.46.27-1.07.64-2.18.64s-1.73-.37-2.18-.64c-.37-.22-.6-.36-1.15-.36-.56 0-.78.13-1.15.36-.46.27-1.08.64-2.19.64-1.11 0-1.73-.37-2.18-.64-.37-.23-.6-.36-1.15-.36s-.78.13-1.15.36c-.46.27-1.08.64-2.19.64v-2c.56 0 .78-.13 1.15-.36.46-.27 1.08-.64 2.19-.64s1.73.37 2.18.64c.37.23.59.36 1.15.36.56 0 .78-.13 1.15-.36.46-.27 1.08-.64 2.19-.64 1.11 0 1.73.37 2.18.64.37.22.6.36 1.15.36s.78-.13 1.15-.36c.45-.27 1.07-.64 2.18-.64s1.73.37 2.18.64c.37.23.59.36 1.15.36v2zm0-4.5c-1.11 0-1.73-.37-2.18-.64-.37-.22-.6-.36-1.15-.36-.56 0-.78.13-1.15.36-.45.27-1.07.64-2.18.64s-1.73-.37-2.18-.64c-.37-.22-.6-.36-1.15-.36-.56 0-.78.13-1.15.36-.45.27-1.07.64-2.18.64s-1.73-.37-2.18-.64c-.37-.22-.6-.36-1.15-.36s-.78.13-1.15.36c-.47.27-1.09.64-2.2.64v-2c.56 0 .78-.13 1.15-.36.45-.27 1.07-.64 2.18-.64s1.73.37 2.18.64c.37.22.6.36 1.15.36.56 0 .78-.13 1.15-.36.45-.27 1.07-.64 2.18-.64s1.73.37 2.18.64c.37.22.6.36 1.15.36s.78-.13 1.15-.36c.45-.27 1.07-.64 2.18-.64s1.73.37 2.18.64c.37.22.6.36 1.15.36v2zM8.67 12c.56 0 .78-.13 1.15-.36.46-.27 1.08-.64 2.19-.64 1.11 0 1.73.37 2.18.64.37.22.6.36 1.15.36s.78-.13 1.15-.36c.12-.07.26-.15.41-.23L10.48 5C8.93 3.45 7.5 2.99 5 3v2.5c1.82-.01 2.89.39 4 1.5l1 1-3.25 3.25c.31.12.56.27.77.39.37.23.59.36 1.15.36z"/><circle cx="16.5" cy="5.5" r="2.5"/><path fill="none" d="M0 0h24v24H0z"/></svg>
       </div>
@@ -16,7 +17,7 @@ const Index = () => (
       <p><a className="btn btn-success" href="signup" role="button">Create An Account</a></p>
       <p><a className="btn btn-success" href="documents" role="button">Manage Your Pools Now</a></p>
     </Jumbotron>
-
+    </div>
    <div className="section2">
       <Row>
         <Col xs={ 12 }>
@@ -129,7 +130,7 @@ const Index = () => (
                 <div className="">
                   <h3 className="font-w-200">Instant Conference Calls</h3>
                   <p className="btm-margin-20 font-w-200">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                  <a class="btn btn-success" href="signup" role="button">Create An Account</a>
+                  <LoginButtons />
                 </div>
               </div>
             </Col>
